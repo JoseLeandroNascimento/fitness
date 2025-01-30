f0dff8fb3898e85cd2687a5d9b47322c9b82859c
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-01-30 11:24:03 -0500

    feat: roteando telas

--- app/src/main/java/com/example/fitness/ImcActivity.kt
+++ app/src/main/java/com/example/fitness/ImcActivity.kt
@@ -1,32 +1,20 @@
 package com.example.fitness
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var btnImc:LinearLayout
-
+class ImcActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_imc)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        btnImc = findViewById<LinearLayout>(R.id.btn_imc)
-
-        btnImc.setOnClickListener {
-            val i = Intent(this, ImcActivity::class.java)
-            startActivity(i)
         }
     }
 }
