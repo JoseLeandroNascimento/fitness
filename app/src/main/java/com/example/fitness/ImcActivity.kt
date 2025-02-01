c9fda7b83c8cba69294190e72f1f5bc6a43d4341
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-01-31 23:52:50 -0500

    feat: validando campos de formulario

--- app/src/main/java/com/example/fitness/ImcActivity.kt
+++ app/src/main/java/com/example/fitness/ImcActivity.kt
@@ -1,12 +1,19 @@
 package com.example.fitness
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,32 @@ class ImcActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend = findViewById<Button>(R.id.btn_imc_send)
+
+        btnSend.setOnClickListener {
+
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+        }
+
+    }
+
+    private fun validate(): Boolean {
+
+        val valueWeight = editWeight.text
+        val valueHeight = editHeight.text
+
+        return (valueWeight.toString().isNotEmpty()
+                && valueHeight.toString().isNotEmpty()
+                && !valueWeight.toString().startsWith("0")
+                && !valueHeight.toString().startsWith("0"))
+
     }
 }
