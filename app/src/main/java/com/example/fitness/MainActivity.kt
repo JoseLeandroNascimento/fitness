6f4abada33b53d545ae78e05260dd0af44eb55e7
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-02-02 01:16:49 -0500

    feat: Iniciando recyclerView

--- app/src/main/java/com/example/fitness/MainActivity.kt
+++ app/src/main/java/com/example/fitness/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.fitness
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnImc:LinearLayout
+
+    private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +25,32 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        btnImc = findViewById<LinearLayout>(R.id.btn_imc)
+        val adapter = MainAdapter()
 
-        btnImc.setOnClickListener {
-            val i = Intent(this, ImcActivity::class.java)
-            startActivity(i)
+        rvMain = findViewById(R.id.rv_main)
+        rvMain.adapter = adapter
+        rvMain.layoutManager = LinearLayoutManager(this)
+
+
+    }
+
+    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
         }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        }
+
+    }
+
+    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
     }
 }
